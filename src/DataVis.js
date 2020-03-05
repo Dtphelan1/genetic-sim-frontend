53872961bf6c4ec6ca0d6b8245bc2c8c4059761c
Dylan Phelan <dtphelan1@gmail.com>
2020-03-04 21:42:48 -0500

    Suppressed warnings and removed newline

--- src/DataVis.js
+++ src/DataVis.js
@@ -7,7 +7,6 @@ export default function DataVis(props) {
     <>
       <p>
         {JSON.stringify(data)}
-
       </p>
     </>
   );
--- src/FrequencySlider.js
+++ src/FrequencySlider.js
@@ -4,7 +4,6 @@ import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import Slider from '@material-ui/core/Slider';
-import Input from '@material-ui/core/Input';
 
 const useStyles = makeStyles({
   root: {
